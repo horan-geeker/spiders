4bbca895b63fd1de2741d6695e50ff683a729235
hejunwei <hejunwei@bluestoneapp.com>
2017-09-16 12:49:11 +0800

    fix path

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from scrapy.cmdline import execute
 import sys
 import os
 
-# print(os.path.dirname(__file__))
 # sys.path.append(os.path.dirname(__file__))
 # execute(["scrapy", "crawl", "jobbole"])
 execute(["scrapy", "crawl", "zhihu"])
